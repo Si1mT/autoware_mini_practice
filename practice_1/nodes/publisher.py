fdacc9f1a4a24e03deef8c0f7f0f95b829728c8a
Siim Tanavots <stanavots@gmail.com>
2024-02-13 15:26:05 +0200

    task 6: edited launch file to run multiple publisher nodes

--- practice_1/nodes/publisher.py
+++ practice_1/nodes/publisher.py
@@ -3,7 +3,8 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
+#rate = rospy.Rate(2)
+rate = rospy.Rate(rospy.get_param('~rate'))
 pub = rospy.Publisher('/message', String, queue_size=10)
 message = rospy.get_param('~message', 'Hello World!')
 
