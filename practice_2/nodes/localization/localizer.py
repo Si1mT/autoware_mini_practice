2f395f321df8828ffc94f72f193e31b10a52b33d
Siim Tanavots <stanavots@gmail.com>
2024-02-20 16:03:56 +0200

    practice 2 task 3 publish current pose

--- practice_2/nodes/localization/localizer.py
+++ practice_2/nodes/localization/localizer.py
@@ -8,7 +8,27 @@ from tf2_ros import TransformBroadcaster
 from pyproj import CRS, Transformer, Proj
 
 from novatel_oem7_msgs.msg import INSPVA
-from geometry_msgs.msg import PoseStamped, TwistStamped, Quaternion, TransformStamped
+from geometry_msgs.msg import PoseStamped, TwistStamped, Quaternion, TransformStamped, Pose, Point
+
+from std_msgs.msg import Header
+
+
+# Task 3
+# convert azimuth to yaw angle
+def convert_azimuth_to_yaw(azimuth):
+    """
+    Converts azimuth to yaw. Azimuth is CW angle from the North. Yaw is CCW angle from the East.
+    :param azimuth: azimuth in radians
+    :return: yaw in radians
+    """
+    yaw = -azimuth + math.pi/2
+    # Clamp within 0 to 2 pi
+    if yaw > 2 * math.pi:
+        yaw = yaw - 2 * math.pi
+    elif yaw < 0:
+        yaw += 2 * math.pi
+
+    return yaw
 
 class Localizer:
     def __init__(self):
@@ -31,17 +51,36 @@ class Localizer:
         self.current_velocity_pub = rospy.Publisher('current_velocity', TwistStamped, queue_size=10)
         self.br = TransformBroadcaster()
 
+        # Task 2
         # Coordinate transformer
         self.transformer = Transformer.from_crs(self.crs_wgs84, self.crs_utm)
         self.origin_x, self.origin_y = self.transformer.transform(utm_origin_lat, utm_origin_lon)
-
-
 
     def transform_coordinates(self, msg):
         x, y = self.transformer.transform(msg.latitude, msg.longitude)
         x, y = x - self.origin_x, y - self.origin_y
         # print(msg.latitude, msg.longitude)
         print('x:   ', x, " y:   ", y)
+
+        # calculate azimuth correction
+        azimuth_correction = self.utm_projection.get_factors(msg.longitude, msg.latitude).meridian_convergence
+        
+        # Convert yaw to quaternion
+        yaw = convert_azimuth_to_yaw(msg.azimuth + azimuth_correction)
+        quat_x, quat_y, z, w = quaternion_from_euler(0, 0, yaw)
+        orientation = Quaternion(quat_x, quat_y, z, w)
+        
+        # header = Header(stamp=msg.stamp, frame_id="map")
+        # point = Point(x, y, msg.height - self.undulation)
+        # pose = Pose(point, orientation)
+        current_pose_msg = PoseStamped()
+        current_pose_msg.header.stamp = msg.header.stamp
+        current_pose_msg.header.frame_id = "map"
+        current_pose_msg.pose.position.x = x
+        current_pose_msg.pose.position.y = y
+        current_pose_msg.pose.position.z = msg.height - self.undulation
+        current_pose_msg.pose.orientation = orientation
+        self.current_pose_pub.publish(current_pose_msg)
 
     def run(self):
         rospy.spin()
