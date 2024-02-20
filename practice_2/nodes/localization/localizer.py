c209b87943dd53bba5034e6f1895a401ef0303af
Siim Tanavots <stanavots@gmail.com>
2024-02-20 15:09:35 +0200

    practice 2 task 2 converted coordinates from lat,long to UTM zone 35N projection

--- practice_2/nodes/localization/localizer.py
+++ practice_2/nodes/localization/localizer.py
@@ -14,9 +14,9 @@ class Localizer:
     def __init__(self):
 
         # Parameters
-        # self.undulation = rospy.get_param('/undulation')
-        # utm_origin_lat = rospy.get_param('/utm_origin_lat')
-        # utm_origin_lon = rospy.get_param('/utm_origin_lon')
+        self.undulation = rospy.get_param('/undulation')
+        utm_origin_lat = rospy.get_param('/utm_origin_lat')
+        utm_origin_lon = rospy.get_param('/utm_origin_lon')
 
         # Internal variables
         self.crs_wgs84 = CRS.from_epsg(4326)
@@ -30,10 +30,18 @@ class Localizer:
         self.current_pose_pub = rospy.Publisher('current_pose', PoseStamped, queue_size=10)
         self.current_velocity_pub = rospy.Publisher('current_velocity', TwistStamped, queue_size=10)
         self.br = TransformBroadcaster()
-        
+
+        # Coordinate transformer
+        self.transformer = Transformer.from_crs(self.crs_wgs84, self.crs_utm)
+        self.origin_x, self.origin_y = self.transformer.transform(utm_origin_lat, utm_origin_lon)
+
+
 
     def transform_coordinates(self, msg):
-        print(msg.latitude, msg.longitude)
+        x, y = self.transformer.transform(msg.latitude, msg.longitude)
+        x, y = x - self.origin_x, y - self.origin_y
+        # print(msg.latitude, msg.longitude)
+        print('x:   ', x, " y:   ", y)
 
     def run(self):
         rospy.spin()
