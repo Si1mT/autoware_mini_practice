9cb4ebe838e76d39fb3bb67fd79e6dc86a404c3f
Siim Tanavots <stanavots@gmail.com>
2024-02-28 19:18:30 +0200

    pratice task 3 added vehicle cmd publisher to pure pursuit follower

--- practice_3/nodes/control/pure_pursuit_follower.py
+++ practice_3/nodes/control/pure_pursuit_follower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import rospy
 
-from autoware_msgs.msg import Lane
+from autoware_msgs.msg import Lane, VehicleCmd
 from geometry_msgs.msg import PoseStamped
 
 class PurePursuitFollower:
@@ -10,6 +10,7 @@ class PurePursuitFollower:
         # Parameters
 
         # Publishers
+        self.vehicle_cmd_pub = rospy.Publisher('/control/vehicle_cmd', VehicleCmd)
 
         # Subscribers
         rospy.Subscriber('path', Lane, self.path_callback, queue_size=1)
@@ -21,6 +22,15 @@ class PurePursuitFollower:
 
     def current_pose_callback(self, msg):
         print('x:', msg.pose.position.x, 'y:', msg.pose.position.y)
+
+        vehicle_cmd = VehicleCmd()
+        vehicle_cmd.header.stamp = msg.header.stamp
+        vehicle_cmd.header.frame_id = "base_link"
+        vehicle_cmd.ctrl_cmd.steering_angle = 0.2
+        vehicle_cmd.ctrl_cmd.linear_velocity = 10.0
+
+        self.vehicle_cmd_pub.publish(vehicle_cmd)
+
 
     def run(self):
         rospy.spin()
